49e3aef92b8199de3aed22c5c38f83baee4ed54a
root <you@example.com>
2022-09-20 12:05:32 -0700

    task 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
-def islower(c):
-    return(ord('a') <= ord(c) <= ord('z'))
+def uppercase(str):
+    for c in str:
+        if ord(c) >= ord('a') and ord(c) <= ord('z'):
+            c = chr(ord(c) - 32)
+        print("{}".format(c), end="")
+    print()
