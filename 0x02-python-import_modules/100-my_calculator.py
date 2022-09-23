499b23da115e01555b173f4f94372a28febea161
root <you@example.com>
2022-09-23 10:49:47 -0700

    task 6

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,43 +3,43 @@
 the file calculator_1.py and handles basic operations.
 """
 if __name__ == "__main__":
-import sys
-from calculator_1 import add, sub, mul, div
-if len(sys.argv) != 4:
-print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-exit(1)
-if sys.argv[2] == "+":
-sum = add(int(sys.argv[1]), int(sys.argv[3]))
-print(
-"{} {} {} = {}".format(
-sys.argv[1],
-sys.argv[2],
-sys.argv[3],
-sum))
-elif sys.argv[2] == "-":
-sub_ = sub(int(sys.argv[1]), int(sys.argv[3]))
-print(
-"{} {} {} = {}".format(
-sys.argv[1],
-sys.argv[2],
-sys.argv[3],
-sub_))
-elif sys.argv[2] == "*":
-mul_ = mul(int(sys.argv[1]), int(sys.argv[3]))
-print(
-"{} {} {} = {}".format(
-sys.argv[1],
-sys.argv[2],
-sys.argv[3],
-mul_))
-elif sys.argv[2] == "/":
-div_ = div(int(sys.argv[1]), int(sys.argv[3]))
-print(
-"{} {} {} = {}".format(
-sys.argv[1],
-sys.argv[2],
-sys.argv[3],
-div_))
-else:
-print("Unknown operator. Available operators: +, -, * and /")
-exit(1)
+    import sys
+    from calculator_1 import add, sub, mul, div
+    if len(sys.argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    if sys.argv[2] == "+":
+        sum = add(int(sys.argv[1]), int(sys.argv[3]))
+        print(
+            "{} {} {} = {}".format(
+                sys.argv[1],
+                sys.argv[2],
+                sys.argv[3],
+                sum))
+    elif sys.argv[2] == "-":
+        sub_ = sub(int(sys.argv[1]), int(sys.argv[3]))
+        print(
+            "{} {} {} = {}".format(
+                sys.argv[1],
+                sys.argv[2],
+                sys.argv[3],
+                sub_))
+    elif sys.argv[2] == "*":
+        mul_ = mul(int(sys.argv[1]), int(sys.argv[3]))
+        print(
+            "{} {} {} = {}".format(
+                sys.argv[1],
+                sys.argv[2],
+                sys.argv[3],
+                mul_))
+    elif sys.argv[2] == "/":
+        div_ = div(int(sys.argv[1]), int(sys.argv[3]))
+        print(
+            "{} {} {} = {}".format(
+                sys.argv[1],
+                sys.argv[2],
+                sys.argv[3],
+                div_))
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
