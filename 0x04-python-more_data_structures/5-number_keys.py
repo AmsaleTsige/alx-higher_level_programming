3f5801a716e184038a7fb8707b6febf0a6a32e8b
root <you@example.com>
2022-09-28 04:54:24 -0700

    Task 5

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def number_keys(a_dictionary):
-    count = 0
-    for key, elem in enumerate(a_dictionary):
-        count += 1
-        return count
+    return len(a_dictionary)
 
