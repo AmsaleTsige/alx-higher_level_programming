3c1e4aed0449f0022d9969f04e31b47a9ac515d9
root <you@example.com>
2022-09-28 11:24:27 -0700

    Squared simple

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    if not matrix:
-        print()
-        return [[item**2 for item in row] for row in matrix]
+    new_matrix = []
+    for row in matrix:
+        transpose = []
+        for elem in row:
+            elem = elem * elem
+            transpose.append(elem)
+            new_matrix.append(transpose)
+            return new_matrix
