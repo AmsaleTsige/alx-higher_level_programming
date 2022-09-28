4b9a2150368c55b02f93b00bbf87783df92060cb
root <you@example.com>
2022-09-28 11:39:32 -0700

    Task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,44 +1,21 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    # Fail checks, none, not a string
-    if not roman_string:
+    if (not isinstance(roman_string, str) or roman_string is None):
         return 0
-    if not isinstance(roman_string, str):
-        return 0
-    if not roman_string.isupper():
-        return 0
-    # Dictionary for roman numerals
-    r_dict = {
-            "I": 1,
-            "IV": 4,
-            "V": 5,
-            "IX": 9,
-            "X": 10,
-            "L": 50,
-            "C": 100,
-            "D": 500,
-            "M": 1000
+    roman = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
             }
-    result = 0
-    temp = list(roman_string)
-    # Concat 4 and 9s
-    if len(temp) > 1:
-        idx = 0
-        for i in temp:
-            try:
-                if temp[idx] == 'I' and temp[idx + 1] == 'V':
-                    temp[idx:idx + 2] = [''.join(temp[idx:idx + 2])]
-            except IndexError:
-                pass
-            try:
-                if temp[idx] == 'I' and temp[idx + 1] == 'X':
-                    temp[idx:idx + 2] = [''.join(temp[idx:idx + 2])]
-            except IndexError:
-                pass
-            idx += 1
-            # Search in dict for correct numbers and add
-            for k, v in r_dict.items():
-                for index in temp:
-                    if index == k:
-                        result += v
-                        return result
+    int_value = 0
+    for i in range(len(roman_string)):
+        if i > 0 and roman[roman_string[i]] > roman[roman_string[i - 1]]:
+            int_value += roman[roman_str]]\
+                    2 * roman[roman_string[i - ing[i - 1]]
+                            else:
+                            int_value += roman[roman_string[i]]
+                            return int_value
