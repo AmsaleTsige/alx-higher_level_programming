10c2f2ad39d4f9ac10c259b6bbad0ab4db391dbc
root <you@example.com>
2022-09-28 04:19:48 -0700

    Search and replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -4,5 +4,4 @@ def search_replace(my_list, search, replace):
     for idx, item in enumerate(new_list):
         if item == search:
             new_list[idx] = replace
-            return new_list
-
+        return new_list
