48fe2d75c552fb1148f245894f124cc3daa6aedc
root <you@example.com>
2022-09-28 05:08:55 -0700

    Print sorted dictionary

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    for key, value in sorted(a_dictionary.items(), key=lambda x: x[0]):
+    for key, value in sorted(a_dictionary.items()):
         print("{}: {}".format(key, value))
-
