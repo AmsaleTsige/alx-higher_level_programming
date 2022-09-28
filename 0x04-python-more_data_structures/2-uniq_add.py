aba5be3e87d5362e4a9847dcc387d564717a6fef
root <you@example.com>
2022-09-28 04:31:39 -0700

    Task 2

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    unique_values = []
-    sum = 0
-    for elem in my_list:
-        if elem in unique_values:
-            continue
-        else:
-            unique_values.append(elem)
-            for item in unique_values:
-                sum += item
-                return sum
+unique_values = []
+sum = 0
+for elem in my_list:
+    if elem in unique_values:
+        continue
+    else:
+        unique_values.append(elem)
+        for item in unique_values:
+            sum += item
+            return sum
+
