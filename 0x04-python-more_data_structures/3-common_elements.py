a09e029c5e056380631415753724b83b5a3678bc
root <you@example.com>
2022-09-28 05:48:27 -0700

    Present in both

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,8 +1,3 @@
-#!/urs/bin/python3
+#!/usr/bin/python3
 def common_elements(set_1, set_2):
-    common = []
-    for item in set_1
-    for elem in set_2:
-        if item == elem:
-            common.append(item)
-            return common
+    return set(set_1) & set(set_2)
