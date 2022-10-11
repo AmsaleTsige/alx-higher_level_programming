ccb15cfe2c554fcb95ad3fee40b9602832767be7
root <you@example.com>
2022-10-11 12:18:37 -0700

    task 3

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,18 +1,30 @@
 #!/usr/bin/python3
-"""Area of a square"""
+"""A module that defines a square """
+
 
 class Square:
-    """Private instance attribute: size
-    Instantiation with area method """
-    def __init__(self, size=0):
-        """Initializes attribute size """
-        if (type(size) is not int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
+    """A class that represents a square"""
 
-        def area(self):
-           """Calculate area of square"""
-           return (self.__size * self.__size)
+    def __init__(self, size=0):
+        """Initializing this square class
+        Args:
+            size: represnets the size of the square defined
+        Raises:
+            TypeError: if size is not integer
+            ValueError: if size is less than zero
+        """
+
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+
+        self.__size = size
+
+    def area(self):
+        """
+        Calculate area of the square
+        Returns: The square of the size
+        """
+
+        return (self.__size ** 2)
