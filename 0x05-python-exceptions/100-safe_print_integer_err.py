03e54c6bd7e1a891d9ce7cc77379a2cc319ef431
root <you@example.com>
2022-10-10 19:52:13 -0700

    task 7

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,15 +1,21 @@
 #!/usr/bin/python3
 # 100-safe_print_integer_err.py
+
 import sys
+
+
 def safe_print_integer_err(value):
     """Prints an integer with "{:d}".format().
+
     If a ValueError message is caught, a corresponding
     message is printed to standard error.
+
     Args:
-    value (int): The integer to print.
+        value (int): The integer to print.
+
     Returns:
-    If a TypeError or ValueError occurs - False.
-    Otherwise - True.
+        If a TypeError or ValueError occurs - False.
+        Otherwise - True.
     """
     try:
         print("{:d}".format(value))
@@ -17,4 +23,3 @@ def safe_print_integer_err(value):
     except (TypeError, ValueError):
         print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
         return (False)
-
