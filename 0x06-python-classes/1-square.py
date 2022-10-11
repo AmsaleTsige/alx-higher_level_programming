875d0972ea36647b07bf0486cea30d83b4cff63c
root <you@example.com>
2022-10-11 03:34:36 -0700

    1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-"""Square class definition"""
+"""Square class definition""
 class Square:
     """Represents a square
     Attributes:
-    __size (int): size of a side of the square
-    """
-    def __init__(self, size):
-    """Initializes a square
-    Args:
-    size (int): size of a side of the square
-    Returns: None
-    """
-    self.__size = size
+        _size (int): size of a side of the square
+        """
+        def _init_(self, size):
+        """Initializes a square
+        Args:
+            size (int): size of a side of the square
+            Returns: None
+            """
+            self._size = size
