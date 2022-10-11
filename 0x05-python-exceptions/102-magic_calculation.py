81737e756c20f4a35da512d07230119ffaffe4fc
root <you@example.com>
2022-10-10 19:58:07 -0700

    task 9

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-# 102-magic_calculation.py
 def magic_calculation(a, b):
     result = 0
     for i in range(1, 3):
@@ -11,4 +10,4 @@ def magic_calculation(a, b):
         except Exception:
             result = b + a
             break
-        return (result)
+    return (result)
