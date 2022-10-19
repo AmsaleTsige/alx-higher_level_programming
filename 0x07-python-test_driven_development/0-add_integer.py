3281c7e0f009c6238ad33e76054e4b4daa2555ab
Amsale Tsige <amsale324@gmail.com>
2022-10-19 20:10:10 +0100

    0x07-python-test_driven_development

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,16 +1,23 @@
 #!/usr/bin/python3
 """
+
 This module has one function that adds up 2 integers
+
 """
+
+
 def add_integer(a, b=98):
     """Return the sum of two integers or floats as integers
+
     Args:
-    a: first argument
-    b: second argument
+        a: first argument
+        b: second argument
+
     Returns:
-    Sum of the two arguments
+        Sum of the two arguments
+
     Raises:
-    TypeError: If either of the arguments not an integer or a float
+        TypeError: If either of the arguments not an integer or a float
     """
     if ((not isinstance(a, int) and not isinstance(a, float))):
         raise TypeError("a must be an integer")
--- 0x07-python-test_driven_development/6-max_integer.py
+++ 0x07-python-test_driven_development/6-max_integer.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 """Module to find the max integer in a list
 """
+
+
 def max_integer(list=[]):
     """Function to find and return the max integer in a list of integers
-    If the list is empty, the function returns None
+        If the list is empty, the function returns None
     """
     if len(list) == 0:
         return None
@@ -12,5 +14,5 @@ def max_integer(list=[]):
     while i < len(list):
         if list[i] > result:
             result = list[i]
-            i += 1
-            return result
+        i += 1
+    return result
