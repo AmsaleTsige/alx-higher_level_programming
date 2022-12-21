442b8743cdad48eb279b555a1288de4a710388a0
root <you@example.com>
2022-12-20 23:13:42 -0800

    0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,10 +3,8 @@
 This script lists all states from the
 database `hbtn_0e_0_usa`.
 """
-
 import MySQLdb
 from sys import argv
-
 if __name__ == '__main__':
     """
     Access to the database and get the states
@@ -14,12 +12,8 @@ if __name__ == '__main__':
     """
     db_connect = MySQLdb.connect(
         host="localhost", user=argv[1], port=3306, passwd=argv[2], db=argv[3])
-
     db_cursor = db_connect.cursor()
-
     db_cursor.execute("SELECT * FROM states")
-
     rows_selected = db_cursor.fetchall()
-
     for row in rows_selected:
         print(row)
